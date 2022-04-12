66ab5bff076472501231e45177a1b50a04c3312b
Adam Weld <weld@valvesoftware.com>
2022-04-12 12:10:15 -0700

    removed dev script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "packaging"
-makepkg -scif
-echo "restarting service"
-sudo systemctl enable jupiter-fan-control.service
-sudo systemctl daemon-reload
-sudo systemctl restart jupiter-fan-control.service
-echo "finished"
