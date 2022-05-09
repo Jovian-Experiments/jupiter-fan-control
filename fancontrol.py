e49f0dc078e79c1505bb336f4b62108bdd261c86
Adam Weld <weld@valvesoftware.com>
2022-05-09 12:01:45 -0700

    removed manual windup setpint

--- fancontrol.py
+++ fancontrol.py
@@ -56,7 +56,6 @@ class Hybrid(object):
 
 class FeedForward(object):
     def __init__(self, Kp, Ki, Kd, windup, A_setpoint, B_setpoint, T_setpoint) -> None:
-        windup = 1000
         self.A_setpoint = A_setpoint
         self.B_setpoint = B_setpoint
         self.T_setpoint = T_setpoint
@@ -176,7 +175,7 @@ class Device(object):
         elif self.type == "hybrid":
             self.controller = Hybrid(float(config["slope"]), float(config["A_setpoint"]), float(config["B_setpoint"]), int(config["T_setpoint"]))
         elif self.type == "feedforward":
-            self.controller = FeedForward(float(config["Kp"]), float(config["Ki"]), float(config["Kd"]), int(config["windup_limit"]), float(config["A_setpoint"]), float(config["B_setpoint"]), int(config["T_setpoint"]))
+            self.controller = FeedForward(float(config["Kp"]), float(config["Ki"]), float(config["Kd"]), float(config["windup_limit"]), float(config["A_setpoint"]), float(config["B_setpoint"]), int(config["T_setpoint"]))
         else:
             print("error loading device controller \n")
             exit(1)
